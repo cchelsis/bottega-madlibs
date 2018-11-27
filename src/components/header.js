174becd88b11f7f8a6b9bdde72f41298e08ae67b
cchelsis <stepforwardinyourlife@gmail.com>
2018-11-27 15:40:55 -0700

    build first functional component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { defaultCipherList } from 'constants';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Bottega Madlibs</h1>
+           
+            <p> Fill out the fields below and click the generate button to see the Made Lib story.</p>
+        </div>
+    )
+}
+
+export default Header;
