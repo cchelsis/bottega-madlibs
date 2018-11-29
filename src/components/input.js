8bd18db710728f8809e2bd0f4a7d72b05ae049ea
cchelsis <stepforwardinyourlife@gmail.com>
2018-11-29 15:34:39 -0700

    added remaining inputs

--- src/components/input.js
+++ src/components/input.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { defaultCipherList } from 'constants';
 
-const input = (title) => {
+const Input = ({title, state, name}, onChange) => {
     return (
-        <div className="input">
-            <input/>
-            <label>{title}</label>
-        </div>
+        <div key={name} className="input">
+        <input name={name} value={state} onChange={onChange}/>
+        <label>{title}</label>
+    </div>
     )
 }
 
-export default input;
+export default Input;
